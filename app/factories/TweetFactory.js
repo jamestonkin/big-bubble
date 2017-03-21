c23a7af221534c921bc434d6faf45776421ef4f8
James Tonkin <jameswtonkin@gmail.com>
2017-03-21 10:01:08 -0500

    added functionality to add tweets to company page

---
+++ app/factories/TweetFactory.js
@@ -0,0 +1,59 @@
+'use strict';
+
+app.factory("TweetFactory", ($q, $http, FBCreds) => {
+
+  // let getTweets = () => {
+  //   var tweets16 = $http.get('../../data/tweets15-16.json'),
+  //       tweets17 = $http.get('../../data/tweets-17.json');
+  //   $q.all([tweets16, tweets17]).then(function(result) {
+  //     var tweets = [];
+  //     angular.forEach(result, function(reponse) {
+  //       tweets.push(response.data);
+  //     });
+  //     return tweets;
+  //   }).then(function(result) {
+  //     $scope.allTweets = result.join(", ");
+  //     console.log("ALL TWEETS: ", $scope.allTweets);
+  //   });
+  // }
+
+  let getTweets16 = () => {
+    return $q((resolve, reject) => {
+      let tweets16 = [];
+      $http.get('../../data/tweets15-16.json')
+      .then((tweetObject) => {
+        let tweetCollection = tweetObject.data;
+        console.log('my tweetCollection16: ', tweetCollection );
+        Object.keys(tweetCollection).forEach((key) => {
+          tweets16.push(tweetCollection[key]);
+        });
+        resolve(tweets16);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+    });
+  };
+
+  let getTweets17 = () => {
+    return $q((resolve, reject) => {
+      let tweets17 = [];
+      $http.get('../../data/tweets-17.json')
+      .then((tweetObject) => {
+        let tweetCollection = tweetObject.data;
+        console.log('my tweetCollection17: ', tweetCollection );
+        Object.keys(tweetCollection).forEach((key) => {
+          tweets17.push(tweetCollection[key]);
+        });
+        resolve(tweets17);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+    });
+  };
+
+
+  return {getTweets16, getTweets17};
+
+}); // End Factory
